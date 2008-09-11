96936b19169ae754282f072b8cb373a69c609581
Giles Bowkett <gilesb@gmail.com>
2008-09-11 15:13:15 -0700

    Dir scanning clean and refactored. Duplication detection messy. Very likely to switch to class approach (from module) in nearish future.

--- lib/towelie.rb
+++ lib/towelie.rb
@@ -1,2 +1,30 @@
+require 'find'
+
 module Towelie
+  def files(dir)
+    accumulator = []
+    Find.find(dir) do |filename|
+      next if File.directory? filename
+      accumulator << filename
+    end
+    accumulator
+  end
+  def duplication?(dir)
+    files(dir) do |filename|
+      next if filename =~ /\.git/
+      # if filename =~ /.+\.e?rb$/
+        File.open(filename, "r") do |file|
+          # previous = current = nil
+          file.each_line do |line|
+            # next if skip?(line)
+            stripped = line.gsub(/\s/, "")
+
+            current = Line.new(line, stripped, previous)
+            @all_lines << current
+            previous = current
+          end
+        end
+      # end
+    end
+  end
 end
--- spec/towelie_spec.rb
+++ spec/towelie_spec.rb
@@ -2,7 +2,12 @@ require 'lib/towelie'
 include Towelie
 
 describe Towelie do
-  it "scans a directory"
-  it "reports duplication"
+  it "scans a directory, returning a list of files" do
+    files("spec/test_data").sort.should == ["spec/test_data/first_file.rb",
+                                            "spec/test_data/second_file.rb"]
+  end
+  it "reports duplication in a set of files" do
+    duplication?("spec/test_data").should be_true
+  end
   it "reports unique code"
 end
