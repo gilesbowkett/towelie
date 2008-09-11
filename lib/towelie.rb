6c12b6bdf645f9cba3fce34be6de45de07bbb385
Giles Bowkett <gilesb@gmail.com>
2008-09-11 14:53:52 -0700

    Trivial rename, created minimal spec.

---
+++ lib/towelie.rb
@@ -0,0 +1,2 @@
+module Towelie
+end
---
+++ spec/towelie_spec.rb
@@ -0,0 +1,8 @@
+require 'lib/towelie'
+include Towelie
+
+describe Towelie do
+  it "scans a directory"
+  it "reports duplication"
+  it "reports unique code"
+end
